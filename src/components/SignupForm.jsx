ba77631a97fa342abc568689ae193e30a2b3cc27
iMorphex <ahmedsarwat3000@yahoo.com>
2023-12-24 17:43:32 +0200

    added signup form validations

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,6 +1,20 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { validateEmail } from "../utils/helpers";
 
 function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSuccess(data) {
+    console.log(data);
+  }
+
+  console.log(errors);
+
   return (
     <div className="mx-auto  flex flex-col items-center justify-center px-6 py-8 md:h-[calc(100vh-5rem)] lg:py-0">
       <div className="w-full rounded-lg bg-gray-200 shadow   sm:max-w-md md:mt-0 xl:p-0">
@@ -8,7 +22,10 @@ function SignupForm() {
           <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-800  md:text-2xl">
             Create and account
           </h1>
-          <form className="space-y-4 md:space-y-6" action="#">
+          <form
+            onSubmit={handleSubmit(onSuccess)}
+            className="space-y-4 md:space-y-5"
+          >
             <div>
               <label
                 htmlFor="email"
@@ -17,13 +34,17 @@ function SignupForm() {
                 Your email
               </label>
               <input
-                type="email"
-                name="email"
-                id="email"
                 className="w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-800 focus:outline-[var(--color-brand-500)] "
                 placeholder="email@example.com"
-                required="asd"
+                {...register("email", {
+                  required: "This field is required",
+                  validate: (value) =>
+                    validateEmail(value) || "Enter correct email field",
+                })}
               />
+              <p className="p-1 text-sm tracking-wide text-red-600 sm:text-xs">
+                {errors.email ? errors.email.message : ""}
+              </p>
             </div>
             <div>
               <label
@@ -34,30 +55,64 @@ function SignupForm() {
               </label>
               <input
                 type="password"
-                name="password"
-                id="password"
                 placeholder="••••••••"
                 className="w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-800 focus:outline-[var(--color-brand-500)] "
-                required=""
+                {...register("password", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 5,
+                    message: "Password must be between (5-8) chars ",
+                  },
+                  maxLength: {
+                    value: 8,
+                    message: "Password must be between (5-8) chars ",
+                  },
+                })}
               />
+              <p className="p-1 text-sm tracking-wide text-red-600 sm:text-xs">
+                {errors.password ? errors.password.message : ""}
+              </p>
             </div>
             <div>
               <label
-                htmlFor="confirm-password"
+                htmlFor="confirm"
                 className="mb-2 block text-sm font-medium text-gray-900 "
               >
                 Confirm password
               </label>
               <input
                 type="password"
-                name="confirm-password"
-                htmlFor="confirm-password"
                 placeholder="••••••••"
                 className="w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-800 focus:outline-[var(--color-brand-500)] "
-                required=""
+                {...register("confirm", {
+                  required: "This field is required",
+                  validate: (value, formValues) =>
+                    value === formValues.password || "Passwords doesnt match !",
+                })}
               />
+              <p className="p-1 text-sm tracking-wide text-red-600 sm:text-xs">
+                {errors.confirm ? errors.confirm.message : ""}
+              </p>
             </div>
-
+            <div className="flex items-center">
+              <input
+                aria-describedby="terms"
+                type="checkbox"
+                className="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 accent-[var(--color-brand-500)] hover:accent-[var(--color-brand-500)] "
+                {...register("terms", {
+                  required: "This field is required",
+                })}
+              />
+              <label className="ml-3 block text-sm font-light text-gray-500 ">
+                I accept the{" "}
+                <a className=" font-medium hover:underline" href="#">
+                  Terms and Conditions
+                </a>
+              </label>
+              <p className="pl-1 text-sm tracking-wide text-red-600 sm:text-xs">
+                {errors.terms ? errors.terms.message : ""}
+              </p>
+            </div>
             <button
               type="submit"
               className=" w-full rounded-lg bg-[var(--color-brand-500)] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[var(--color-brand-600)] focus:outline-none focus:ring-4"
